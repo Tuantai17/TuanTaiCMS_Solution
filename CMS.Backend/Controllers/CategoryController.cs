52058316f31f4029e2f4b28d8bc6d286fc3cb5c1
Tuantai17 <tuantainguyen13579@gmail.com>
2026-05-15 16:24:16 +0700

    hoan thanh buoi 2 ket noi duoc voi SQL SERVER

--- CMS.Backend/Controllers/CategoryController.cs
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc;
-using CMS.Data.Entities; // Kết nối tới lớp dữ liệu bạn vừa tạo
+using CMS.Data;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        // Tạo danh sách dữ liệu mẫu trực tiếp trong code
-        var list = new List<Category> {
-            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
-            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
-        };
-        return View(list); // Gửi danh sách này sang giao diện
+        var data = _context.Categories.ToList();
+        return View(data);
     }
 }
