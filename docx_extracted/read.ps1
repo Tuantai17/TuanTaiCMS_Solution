6ec2c8c152d705ea6ae2b1cef52135686953cfdf
Tuantai17 <tuantainguyen13579@gmai.com>
2026-06-20 10:08:43 +0700

    clean: remove temporary files, changelogs, and build files from tracking

--- docx_extracted/read.ps1
+++
@@ -1,3 +0,0 @@
-[xml]$xml = Get-Content -Path "e:\ASP.NET\TuanTaiCMS_Solution\docx_extracted\word\document.xml" -Encoding UTF8
-$text = $xml.document.body.InnerText
-[System.IO.File]::WriteAllText("e:\ASP.NET\TuanTaiCMS_Solution\docx_extracted_content.txt", $text, [System.Text.Encoding]::UTF8)
